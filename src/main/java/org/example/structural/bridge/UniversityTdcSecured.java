f61ee0299258c2785a1f678877bae2e10281cb89
devdan <f.gomez@univermilenium.edu.mx>
2025-01-25 00:04:33 -0600

    primera solucion aplicando herencia en dos dimensiones independientes

---
+++ src/main/java/org/example/structural/bridge/UniversityTdcSecured.java
@@ -0,0 +1,14 @@
+package org.example.structural.bridge;
+
+public class UniversityTdcSecured implements CreditCardPayment{
+
+    /**
+     * Se intenta extender de la clase Payment en dos dimensiones independientes
+     * una para definir el tipo de tdc y otra si es de tipo seguro o no
+     */
+    @Override
+    public void executePay() {
+
+    }
+
+}
